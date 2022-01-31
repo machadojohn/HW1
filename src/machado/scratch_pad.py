6f2531b3c59530484af93592cdce7197ba592636
jrmachado <jrmachado@electro-matic.com>
2022-01-30 23:05:18 -0500

    reconstructed after pull

--- src/machado/scratch_pad.py
+++ src/machado/scratch_pad.py
@@ -34,8 +34,13 @@ class Plotter:
             hue = df.columns[2])
 
     def samples_by_category_with_decision_boundary(self, df, theta):
+        if theta[2] == 0: return
         _p = self.samples_by_category(df)
-        plt.plot([-12, -4], [2, 4], linewidth=2)
+        _feature_1_range = [df.x.min(), df.x.max()]
+        _slope = -theta[1]/theta[2]
+        _intercept = -theta[0]/[theta[2]]
+        _feature_2_range = _slope * _feature_1_range + _intercept
+        plt.plot(_feature_1_range, _feature_2_range, linewidth=2)
 
 class RandomData: 
 
@@ -64,30 +69,30 @@ class Perceptron:
     def train(self):
         for _ in range(self.epochs):
             for _i, _sample_i in enumerate(self.samples.iterrows()):
-                print(self.samples.iloc[_i,:])
+                self.neuron(_i, np.array([_sample_i[1]['x'], _sample_i[1]['y']]))
                 self.misclassified_counts[_i] = 0
 
     def neuron(self, index, sample):
-        _prediction = self.predict(sample)
-        self.evaluate_prediction(index, sample, _prediction)
+        _processed_sample = np.insert(sample, 0, 1).reshape(-1,1)
+        _prediction = self.predict(_processed_sample)
+        self.evaluate_prediction(index, _processed_sample, _prediction)
     
     def predict(self, sample):
-        _processed_sample = np.insert(sample, 0, 1).reshape(-1,1)
-        _dot_product = np.dot(_processed_sample.T, _theta)
+        _dot_product = np.dot(sample.T, self.theta)
         _prediction = self.activation_function.check(_dot_product)
         return _prediction
     
     def evaluate_prediction(self, index, sample, prediction):
-        if np.squeeze(prediction, self.labels[index]) == 0:
+        if np.squeeze(prediction) - self.labels[index] == 0:
             return
         self.update_theta(index, sample, prediction)
         self.increment_misclassification_count(index)
 
     def update_theta(self, index, sample, prediction):
-        self.theta += self.learning_rate*sample*(self.labels(index) - prediction)
+        self.theta += self.learning_rate*sample*(self.labels[index] - np.squeeze(prediction))
 
     def increment_misclassification_count(self, index):
-        self.misclassified_count[index] += 1
+        self.misclassified_counts[index] += 1
 
     def plot_boundary(self):
         self.plotter.samples_by_category_with_decision_boundary(self.data, self.theta)
@@ -107,7 +112,7 @@ class ActivationFunction:
         self.threashold = threashold
 
     def check(self, data):
-        return [activate(_x) if _x > self.threashold else deactivate(_x) for _x in data]
+        return [self.activate(_x) if _x > self.threashold else self.deactivate(_x) for _x in data]
 
     def activate(self, data):
         return 1.0
@@ -115,8 +120,10 @@ class ActivationFunction:
     def deactivate(self, data):
         return 0.0
 
+
 _data = RandomData(555)
 _perceptron = Perceptron(_data.df)
 _perceptron.epochs = 10
 _perceptron.train()
+print(_perceptron.theta)
 _perceptron.plot_boundary()
